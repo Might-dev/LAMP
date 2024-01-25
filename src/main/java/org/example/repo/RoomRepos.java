8384444191bad67f018fedce4f24f80845d7644c
Might <omegaclassic.official@gmail.com>
2024-01-25 22:29:59 +0300

    Добавил дизайн + auto poll

--- src/main/java/org/example/repo/RoomRepos.java
+++
@@ -1,8 +0,0 @@
-package org.example.repo;
-
-import org.example.domain.Room;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoomRepos extends CrudRepository<Room, Long> {
-
-}
